ac09c2adc607d4f5bf25e8e652ff9f66807ac970
Via5K <1neerajnautiyal@gmail.com>
2022-08-10 02:04:31 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,99 @@
+const express = require("express");
+const parser = require("body-parser");
+const ejs = require("ejs");
+const lodash = require("lodash");
+const dotEnv = require("dotenv").config();
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(parser.urlencoded({
+    extended: true
+}));
+// const url = process.ADMIN + process.env.PASSWORD + process.env.DB;
+const url = "mongodb://localhost:27017/Secrets";
+mongoose.connect(url, {
+    useNewUrlParser: true
+});
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+userSchema.plugin(encrypt, {
+    secret: process.env.SECRET,
+    encryptedFields: ["password"]
+});
+const User = mongoose.model("user", userSchema);
+app.get("/", function(req, res) {
+    res.render("home")
+});
+app.get("/login", function(req, res) {
+    res.render("login")
+});
+app.post("/login", function(req, res) {
+    const email = req.body.username;
+    const password = req.body.password;
+    User.findOne({
+        email: email,
+    }, function(err, result) {
+        if (err) console.log(err);
+        else {
+            if (result) {
+                if (result.password === password) {
+                    console.log("Successfull Login!!");
+                    res.redirect("/secrets");
+                } else {
+                    console.log("Wrong Password");
+                    res.redirect("/");
+                }
+            }
+        }
+
+    });
+});
+app.get("/secrets", function(req, res) {
+    res.render("secrets");
+});
+app.get("/register", function(req, res) {
+    res.render("register")
+});
+app.post("/register", function(req, res) {
+
+    const email = req.body.username;
+    const password = req.body.password;
+    const user = new User({
+        email: email,
+        password: password
+    });
+    //find the user
+    User.find({
+        email: email
+    }, function(err, result) {
+        if (result.length > 0) { //if the user is already in the database, then display this msg and goto login
+            console.log("User Already in the Database Please Login!!");
+            res.redirect("/login");
+        } else if (err) { //if there is an error then goto home register page
+            console.log("Some Error Occurred!!");
+            res.redirect("/register");
+        } else { //and if there was no error and no user then register this user.
+            //add into databse...
+            user.save((err) => {
+                if (!err) {
+                    console.log("Successfully registered!");
+                    res.render("secrets");
+                } else {
+                    console.log(err);
+                }
+            });
+        }
+    });
+});
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Starting Server on 3000");
+});
